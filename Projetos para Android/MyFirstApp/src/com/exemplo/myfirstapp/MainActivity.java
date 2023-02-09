2bf44d4be6422614b6bd726e3fa1f18e7055925d
Tiago Nascimento <49621642+TheNascimento@users.noreply.github.com>
2023-02-09 11:45:06 -0300

    Add files via upload

---
+++ Projetos para Android/MyFirstApp/src/com/exemplo/myfirstapp/MainActivity.java
@@ -0,0 +1,57 @@
+/*
+ * Projeto baseado nas aulas de treinamento do site http://www.developer.android.com 
+ * Autor: Tiago Luiz em 22/10/14
+ */
+
+package com.exemplo.myfirstapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.content.Intent;
+
+
+public class MainActivity extends ActionBarActivity {
+	public final static String EXTRA_MESSAGE = "com.exemplo.myfirstapp.MESSAGE";
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	// Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
+    }
+}
